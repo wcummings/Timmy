a7b9a7a575a68492cd07cb0b550b03181bd86c92
William Cummings <will@wpc.io>
2020-02-14 20:02:05 -0500

    Use bindValue instead of bindParam

--- scoreboard.php
+++ scoreboard.php
@@ -8,49 +8,59 @@ class Scoreboard {
 
     function __construct() {
         $this->db = new SQLite3(self::DB_FILENAME);
+        $this->db->enableExceptions(true);
     }
 
     function recordGame($playerNicknames, $winnerNickname, $comment = "") {
-        $this->db->exec('BEGIN');
-        // TODO: wrap remaining method in try/catch and rollback on error
+        $this->withTransaction(function () use ($playerNicknames, $winnerNickname, $comment) {
+            $this->executeQueryWithParameters(self::INSERT_GAME_QUERY, ['comment' => $comment]);
 
-        $this->executeQueryWithParameters(self::INSERT_GAME_QUERY, ['comment' => $comment]);
+            $gameID = $this->db->lastInsertRowID();
+            $playerIDMap = $this->getPlayerIDMap();
 
-        $gameID = $this->db->lastInsertRowID();
-        $playerIDMap = $this->getPlayerIDMap();
+            if (!in_array($winnerNickname, $playerNicknames)) {
+                throw new Exception('Winner does not appear in list of players');
+            }
 
-        if (!in_array($winnerNickname, $playerNicknames)) {
-            throw new Exception('Winner does not appear in list of players');
-        }
+            $invalidNicknames = array_filter($playerNicknames, function ($nickname) use($playerIDMap) {
+                return !array_key_exists($nickname, $playerIDMap);
+            });
 
-        $invalidNicknames = array_filter($playerNicknames, function ($nickname) use($playerIDMap) {
-            return !array_key_exists($nickname, $playerIDMap);
+            if (count($invalidNicknames) > 0) {
+                throw new Exception("Could not record result, the following players are not registered: "
+                                    . implode($invalidNicknames, ", "));
+            }
+
+            foreach ($playerNicknames as $nickname) {
+                $this->executeQueryWithParameters(self::INSERT_LINE_ITEM_QUERY, [
+                    'game_id' => $gameID,
+                    'player_id' => $playerIDMap[$nickname],
+                    'is_winner' => $nickname === $winnerNickname
+                ]);
+            }
         });
-
-        if (count($invalidNicknames) > 0) {
-            throw new Exception("Could not record result, the following players are not registered: "
-                                . implode($invalidNicknames, ", "));
-        }
-
-        foreach ($playerNicknames as $nickname) {
-            $this->executeQueryWithParameters(self::INSERT_LINE_ITEM_QUERY, [
-                'game_id' => $gameID,
-                'player_id' => $playerIDMap[$nickname],
-                'is_winner' => $nickname === $winnerNickname
-            ]);
-        }
-
-        $this->db->exec('COMMIT');
     }
 
     private function executeQueryWithParameters($query, $params) {
         $statement = $this->db->prepare($query);
         foreach ($params as $key => $value) {
-            $statement->bindParam(":" . $key, $value);
+            $statement->bindValue(":" . $key, $value);
         }
+
         return $statement->execute();
     }
     
+    private function withTransaction($fn) {
+        $this->db->exec('BEGIN');
+        try {
+            $fn();
+            $this->db->exec('COMMIT');
+        } catch (Exception $e) {
+            $this->db->exec('ROLLBACK');
+            throw $e;
+        }
+    }
+
     private function getPlayerIDMap() {
         $result = $this->db->query('SELECT * FROM players');
         $map = [];
